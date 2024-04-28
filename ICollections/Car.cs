2b3cf2535d7d7e74733b5963ee6aa880012b6373
Abdou144 <mohamedabdou1444@gmail.com>
2024-04-28 14:07:03 +0300

    intial

---
+++ ICollections/Car.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICollections
+{
+    public class Car:IComparable
+    {
+
+
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public decimal Price { get; set; }
+        public override int GetHashCode()
+        {
+            int key = 7;
+            int hashCode=key*3 +Id.GetHashCode();
+            hashCode=hashCode*3 +Model.GetHashCode();
+
+            return hashCode;
+
+        }
+        public override bool Equals(object? obj)
+        {
+            if (obj is null)
+                return false;
+            if (obj is not Car car)
+                return false;
+            return this.Equals(car.Id) && this.Equals(car.Model);
+        }
+        public override string ToString()
+        {
+            return $"Id : {this.Id} Model : {this.Model} Price : {this.Price}";
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (object.ReferenceEquals(this,obj))
+                return 0;
+            if (obj is null)
+                return -1;
+            return this.Id.CompareTo(((Car)obj).Id);
+        }
+    }
+}
---
+++ ICollections/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ICollections;
+
+List<Car> carList = new List<Car>()
+            {
+                new Car { Id = 1, Model = "Toyota Camry", Price = 25000 },
+                new Car { Id = 2, Model = "Honda Civic", Price = 22000 },
+                new Car { Id = 3, Model = "Ford Mustang", Price = 35000 },
+                // Add more cars here...
+                new Car { Id = 28, Model = "Tesla Model S", Price = 80000 },
+                new Car { Id = 29, Model = "Chevrolet Silverado", Price = 40000 },
+                new Car { Id = 30, Model = "Jeep Wrangler", Price = 35000 }
+            };
+
+HashSet<Car> Cars = new HashSet<Car>(carList);
+
+List<Car> anotherCarList = new List<Car>()
+            {
+                new Car { Id = 4, Model = "Nissan Altima", Price = 27000 },
+                new Car { Id = 5, Model = "Chevrolet Cruze", Price = 20000 },
+                new Car { Id = 6, Model = "BMW 3 Series", Price = 40000 }
+                // Add more cars here...
+            };
+
+
+
+HashSet<Car> Cars2 = new HashSet<Car>(anotherCarList);
+Cars2.UnionWith(Cars);
+foreach (var item in Cars2)
+{
+    Console.WriteLine(item);
+}
+SortedSet<Car> orderedset = new SortedSet<Car>(Cars2);
+Console.WriteLine("|||||||||||||||||||||||||||||||||||||||||||||||||");
+foreach (var item in orderedset)
+{
+    Console.WriteLine(item);
+}
+LinkedList<Car> LinkedCars = new LinkedList<Car>(orderedset);
+LinkedListNode<Car> node = new LinkedListNode<Car>(new Car { Id = 500, Model = "Lamborgeny", Price = 400000 });
+LinkedCars.AddAfter(LinkedCars.First.Next, node);
+Console.WriteLine("Linked List");
+foreach (var item in LinkedCars)
+{
+    Console.WriteLine(item);
+}
+Console.WriteLine("Hello, World!");
+Console.ReadKey(); 
